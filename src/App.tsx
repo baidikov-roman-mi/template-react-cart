9ffa89e8f8aa44cd0d7f43a1ea3c1be603da5e50
baidikov-roman-mi <wolkdem@gmail.com>
2024-02-17 00:23:24 +0200

    (update): create cart, add products

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
+
+import { ShoppingCartIcon } from "@heroicons/react/24/outline"
+
+import Cart from "./components/Cart.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeCart, setActiveCart] = useState(true)
+
+  function deriveActiveCart() {
+    setActiveCart((prevActiveCart) => !prevActiveCart)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShoppingCartIcon
+        className="h-16 w-16 absolute top-5 left-5 bg-gray-600 p-2 rounded-xl"
+        aria-hidden="true"
+        onClick={deriveActiveCart}
+      />
+      <Cart clickedCartIcon={activeCart} />
     </>
   )
 }
---
+++ src/assets/products.ts
@@ -0,0 +1,46 @@
+export const products = [
+  {
+    id: 1,
+    name: "Logitech G29 Driving Force",
+    href: "#",
+    color: "PC",
+    price: 209.0,
+    quantity: 1,
+    imageSrc:
+      "https://resource.logitechg.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/gaming/en/products/drivingforce/g920-gallery-3-1.png?v=1",
+    imageAlt: "Logitech G29 Driving Force"
+  },
+  {
+    id: 2,
+    name: `Lenovo IdeaPad Pro 5 R7-7/16/1000/4050 16"`,
+    href: "#",
+    color: "Black",
+    price: 1599.0,
+    quantity: 1,
+    imageSrc:
+      "https://www.gigantti.fi/image/dv_web_D1800010021501550/616628/lenovo-ideapad-pro-5-r7-71610004050-16-pelikannettava--pdp_zoom-3000--pdp_main-960.jpg",
+    imageAlt: `Lenovo IdeaPad Pro 5 R7-7/16/1000/4050 16"`
+  },
+  {
+    id: 3,
+    name: "Gaming Table Arena",
+    href: "#",
+    color: "Black",
+    price: 319.0,
+    quantity: 1,
+    imageSrc:
+      "https://static.elisa.com/v2/image/2tqybbhjs47b/3CkohGXixQiqKYbabBNQ02/Arena+-pelip%C3%B6yt%C3%A4+Kokomusta.jpeg?w=800&fm=avif&env=master&q=90",
+    imageAlt: "Gaming Table Arena"
+  },
+  {
+    id: 4,
+    name: "Purple Japan Desk Mat",
+    href: "#",
+    color: "Purple",
+    price: 67.89,
+    quantity: 1,
+    imageSrc:
+      "https://i.etsystatic.com/24922469/r/il/3e4810/3357317711/il_794xN.3357317711_714e.jpg",
+    imageAlt: "Japan Desk Mat"
+  }
+]
